54ff5f86b860557f1bee9cf6e1ab316e592c9eab
Anirudhabg <anirudhabg@gmail.com>
2024-11-05 06:00:45 +0530

    Made Changes in Prgram 2

--- Program 2/playground.py
+++ Program 2/playground.py
@@ -1,5 +1,5 @@
 import time
-import RPi.GPIO as gpio 
+import RPi.GPIO as gpio
 
 gpio.setwarnings(False)
 gpio.setmode(gpio.BOARD)
@@ -10,24 +10,29 @@ led_two = 13
 switch_one = 37
 switch_two = 35
 
+# Defining Output
 gpio.setup(led_one, gpio.OUT)
-gpio.setup(switch_one, gpio.IN)
 gpio.setup(led_two, gpio.OUT)
+
+# Defining Input
+gpio.setup(switch_one, gpio.IN)
 gpio.setup(switch_two, gpio.IN)
+
 
 def glow_led(event):
     print(event)
     if event == switch_one:
-        gpio.output(led_one,False)
+        gpio.output(led_one, False)  # LED is ON
         time.sleep(3)
-        gpio.output(led_one,True)
+        gpio.output(led_one, True)  # LED is OFF
     else:
-        gpio.output(led_two,False)
+        gpio.output(led_two, False)  # LED is ON
         time.sleep(3)
-        gpio.output(led_two,True)
+        gpio.output(led_two, True)  # LED is OFF
 
-gpio.add_event_detect(switch_one, gpio.RISING, callback = glow_led)
-gpio.add_event_detect(switch_two, gpio.RISING, callback = glow_led)
+
+gpio.add_event_detect(switch_one, gpio.RISING, callback = glow_led)  # Event Handler
+gpio.add_event_detect(switch_two, gpio.RISING, callback = glow_led)  # Event Handler
 
 try:
     while True:
